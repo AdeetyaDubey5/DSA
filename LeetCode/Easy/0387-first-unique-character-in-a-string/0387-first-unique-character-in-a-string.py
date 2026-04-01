5f21e5414099c3d2cae1ded59728a3779e4bcff7
Adeetya Dubey <121001727+AdeetyaDubey5@users.noreply.github.com>
2026-04-01 11:48:08 +0530

    0387-first-unique-character-in-a-string

--- LeetCode/Easy/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ LeetCode/Easy/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -5,10 +5,14 @@ class Solution:
         for i in s:
             hsh[i] = hsh.get(i,0) + 1
 
-        for k , v in hsh.items():
-            if v == 1:
-                return s.index(k)
-                break
+        # for k , v in hsh.items():
+        #     if v == 1:
+        #         return s.index(k)
+        #         break
+
+        for i , ch in enumerate(s):
+            if hsh[ch] == 1:
+                return i
 
         return -1
         
